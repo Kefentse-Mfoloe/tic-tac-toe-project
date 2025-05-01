c85e5f83f5d3d480b3c51f4f93e28821338ff732
Kefentse-Mfoloe <kefentse.mfoloe@gmail.com>
2025-05-01 16:31:37 +0200

    Add GameBoard component

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
+export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    
+    function handleSelectSquare(rowIndex, columnIndex) {
+        setGameBoard((prevGameBoard) => {
+            const newGameBoard = [...prevGameBoard].map(row => [...row]);
+            newGameBoard[rowIndex][columnIndex] = 'X';
+            return newGameBoard;
+        })
+    }
+
+    return (
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                       {row.map((playerSymbol, columnIndex) => (
+                        <li key={columnIndex}>
+                            <button onClick={() => handleSelectSquare(rowIndex, columnIndex)}>{playerSymbol}</button>
+                        </li>
+                       ))} 
+                    </ol>
+                </li>
+            ))}
+        </ol>
+    );
+}
